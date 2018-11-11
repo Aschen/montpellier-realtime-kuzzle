45ec9019354cad407b10211716a5e180f7147f2d
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-11-11 17:57:51 +0100

    stops ok

--- test.rb
+++ test.rb
@@ -1,18 +1,25 @@
 require 'json'
 
-stops = JSON.parse(File.read('./stops.json'))
-coords = JSON.parse(File.read('./stops_api.json'))
+content = JSON.parse(File.read('./stops_index.json'))
 
-
-coords.each do |coord|
-  matchs = stops.keep_if { |e| e['name'] == coord['name'] }
-  if matchs
-    matchs.each do |match|
-      match['coordinates'] = coord['coordinates']
-    end
-  else 
-    puts coord
-  end
+stops = content.map do |k, v|
+  v
+end.uniq do |el|
+  el['name']
 end
 
-File.write('stops.json', stops.to_json)
+File.write('./stops.json', stops.to_json)
+# stops = JSON.parse(File.read('./stops.json'))
+# coords = JSON.parse(File.read('./stops_api.json'))
+
+# coords.each do |coord|
+#   matchs = stops.keep_if { |e| e['name'] == coord['name'] }
+
+#   if matchs
+#     matchs.each do |match|
+#       match['coordinates'] = coord['coordinates']
+#     end
+#   end
+# end
+
+# File.write('stops.json', stops.to_json)
